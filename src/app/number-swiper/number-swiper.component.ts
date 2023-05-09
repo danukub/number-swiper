42da4c834a6b7f68d94daf1d15d0afbd24068c33
danukub <danukub@gmail.com>
2023-05-09 16:43:50 +0200

    set  value and scroll to value

--- src/app/number-swiper/number-swiper.component.ts
+++ src/app/number-swiper/number-swiper.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'number-swiper',
@@ -6,11 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./number-swiper.component.css']
 })
 export class NumberSwiperComponent {
-  
+
+  @ViewChild('window') window: ElementRef<HTMLElement> | undefined;
+
   min: number = 0;
   max: number = 99;
   length: number = this.max + 1 - this.min;
   value?: number;
-  
-  blocks = Array(this.length).fill(null).map((x, index) => (index + this.min).toString().padStart(2, '0'));
+  blocks: any;
+
+  inputValue: number = 0;
+
+
+  ngOnInit() {
+    this.blocks = Array(this.length).fill(null).map((x, index) => (index + this.min).toString().padStart(2, '0'));
+    console.log(this.blocks[4])
+  }
+
+  setValue(): void {
+    this.window?.nativeElement.children[this.inputValue].scrollIntoView();
+  }
+
 }
